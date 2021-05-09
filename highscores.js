806f5617cc6a95a5b0ae0b487c5343cf5c789a47
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-05-08 22:01:17 -0500

    added property in highscores javascript to retrieve local storage

--- highscores.js
+++ highscores.js
@@ -8,3 +8,17 @@ clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 });
+// Retreives local stroage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
