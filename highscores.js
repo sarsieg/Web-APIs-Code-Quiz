0e4c959c123c75628d1539bb05fc6b5761dbaf0b
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-05-08 21:59:14 -0500

    added event listener in highscores javascript.

--- highscores.js
+++ highscores.js
@@ -2,3 +2,9 @@
 var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
+
+// Event listener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
