77d1284718233b202b2fa8e379ae670a675f2a5d
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-05-08 23:00:08 -0500

    added event listener to capture initials and local storage for score and initials.

--- questions.js
+++ questions.js
@@ -169,3 +169,33 @@ function allDone() {
 
   questionsDiv.appendChild(createSubmit);
 
+ // Event listener to capture initials and local storage for initials and score
+ createSubmit.addEventListener("click", function () {
+    var initials = createInput.value;
+
+    if (initials === null) {
+
+        console.log("No value entered!");
+
+    } else {
+        var finalScore = {
+            initials: initials,
+            score: timeRemaining
+        }
+        console.log(finalScore);
+        var allScores = localStorage.getItem("allScores");
+        if (allScores === null) {
+            allScores = [];
+        } else {
+            allScores = JSON.parse(allScores);
+        }
+        allScores.push(finalScore);
+        var newScore = JSON.stringify(allScores);
+        localStorage.setItem("allScores", newScore);
+        // Travels to final page
+        window.location.replace("./HighScores.html");
+    }
+});
+
+} 
+
