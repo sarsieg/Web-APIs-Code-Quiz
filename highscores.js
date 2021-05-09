3c30766ce263be7793a12282e898ec41dd6541c5
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-05-08 23:27:20 -0500

    changed link

--- highscores.js
+++ highscores.js
@@ -24,5 +24,5 @@ if (allScores !== null) {
 }
 // Event listener to move to index page
 goBack.addEventListener("click", function () {
-    window.location.replace("index.html");
+    window.location.replace("./index.html");
 });
