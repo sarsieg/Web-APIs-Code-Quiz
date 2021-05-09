aee1cbd715c63c8ab59a48bda87ecf5630d03176
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-05-08 22:03:52 -0500

    added let with array and object for questions

---
+++ questions.js
@@ -0,0 +1,29 @@
+// Var with array and object for questions 
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "3. parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        answer: "3. quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["1. Javascript", "2. terminal / bash", "3. for loops", "4. console log"],
+        answer: "4. console log"
+    },
+
+];
